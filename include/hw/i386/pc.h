01f425f8a1863279dcde186ca830bc8bb896caa7
Sean Christopherson <sean.j.christopherson@intel.com>
2020-08-27 12:12:01 -0700

    target/i386: Add machine option to disable PIC/8259
    Add a machine option to disable the legacy PIC (8259), which cannot be
    supported for TDX guests as TDX-SEAM doesn't allow directly interrupt
    injection.  Using posted interrupts for the PIC is not a viable option
    as the guest BIOS/kernel will not do EOI for PIC IRQs, i.e. will leave
    the vIRR bit set.
    
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>

--- include/hw/i386/pc.h
+++ include/hw/i386/pc.h
@@ -45,6 +45,7 @@ typedef struct PCMachineState {
     bool sata_enabled;
     bool pit_enabled;
     bool hpet_enabled;
+    bool pic_enabled;
     bool default_bus_bypass_iommu;
     uint64_t max_fw_size;
 
@@ -61,6 +62,7 @@ typedef struct PCMachineState {
 #define PC_MACHINE_SMBUS            "smbus"
 #define PC_MACHINE_SATA             "sata"
 #define PC_MACHINE_PIT              "pit"
+#define PC_MACHINE_PIC              "pic"
 #define PC_MACHINE_MAX_FW_SIZE      "max-fw-size"
 /**
  * PCMachineClass:
