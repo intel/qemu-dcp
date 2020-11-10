d964a5bcdfb19e8205677ca89dab4325187b6e2b
Liu Yi L <yi.l.liu@intel.com>
2020-11-10 11:50:14 -0500

    intel_iommu: Fix a bug for scalable mode vIOMMU live migration support
    iq_dw should be migrated to destination.
    
    Signed-off-by: Liu Yi L <yi.l.liu@intel.com>

--- hw/i386/intel_iommu.c
+++ hw/i386/intel_iommu.c
@@ -5092,6 +5092,7 @@ static const VMStateDescription vtd_vmstate = {
         VMSTATE_BOOL(qi_enabled, IntelIOMMUState),
         VMSTATE_BOOL(intr_enabled, IntelIOMMUState),
         VMSTATE_BOOL(intr_eime, IntelIOMMUState),
+        VMSTATE_BOOL(iq_dw, IntelIOMMUState),
         VMSTATE_END_OF_LIST()
     }
 };
