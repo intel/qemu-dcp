a902f5c032910a3751a7e9797097ff6bec2d40e6
Yi Sun <yi.y.sun@linux.intel.com>
2022-01-14 00:57:49 +0800

    intel_iommu: remove ERS bit because HW removes it
    Signed-off-by: Yi Sun <yi.y.sun@linux.intel.com>

--- hw/i386/intel_iommu.c
+++ hw/i386/intel_iommu.c
@@ -6302,7 +6302,7 @@ static void vtd_init(IntelIOMMUState *s)
     } else if (s->scalable_mode && s->scalable_modern) {
         s->ecap |= VTD_ECAP_SMTS | VTD_ECAP_SRS | VTD_ECAP_PASID |
                    VTD_ECAP_PSS(VTD_PASID_SS) | VTD_ECAP_VCS  | VTD_ECAP_PRS |
-                   VTD_ECAP_EAFS | VTD_ECAP_ERS;
+                   VTD_ECAP_EAFS;
         if (s->aw_bits == VTD_HOST_AW_48BIT) {
             s->ecap |= VTD_ECAP_FLTS;
             s->cap |= VTD_CAP_FL1GP;
